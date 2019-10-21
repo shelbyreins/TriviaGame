6204435305c6e40bc312aa18928ea280f4d5388c
Shelby Reinsimar <shelbyreins@yahoo.com>
2019-10-21 11:41:42 -0700

    Add images and start pseduocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+
+//Trivia Game
+//Start game
+    //Use jQuery to start game when we clock the start button
+    //Hide the start button on the following pages
+//Question page
+    //10 multiple choice quesions 
+    //Add a variable of trivia Questions in an array
+        //Add question as a string
+        //Add options as an array
+        //Add answer from the options array
+    //With timers for each question & in between questions
+    //On.clicks for the answers
+    //consitionals if winner or else if show the correct answer or else for unanswered
+    
+//Add in a for loop?? somehwere
+
+//Solutions page 
+    // Add in images/gifs in an array to make them easy to call on
+    //Add in "Correct" or "Incorrect" or "Ran out of time"
+
+    
